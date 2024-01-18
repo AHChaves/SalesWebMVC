06ed1097214a3539ea09fa08f4e9ef1dafa19808
hashi012 <arthurhenri2006@gmail.com>
2024-01-18 16:23:01 -0300

    Other entities and second migration

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using SalesWebMVC.Data;
+using SalesWebMVC.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
